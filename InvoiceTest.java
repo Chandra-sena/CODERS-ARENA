1800ff9b337481ce5691de0737d2cfa3b352c495
Chandra-sena <113082045+Chandra-sena@users.noreply.github.com>
2022-09-11 12:36:18 +0530

    Delete InvoiceTest.java

--- InvoiceTest.java
+++
@@ -1,9 +0,0 @@
-public class InvoiceTest {
-    public static void main(String args[]) {
-        Invoice ob = new Invoice("a4", "paper", 100, 25000);
-        System.out.println(ob.getPartNumber());
-        System.out.println(ob.getPartDesc());
-        System.out.println(ob.getInvoiceAmount());
-
-    }
-}
